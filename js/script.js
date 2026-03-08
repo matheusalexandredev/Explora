7627cd1e40974064f87c6fb5be56c7a2c39a208e
Matheus Alexandre <matheusalexandre.dev@gmail.com>
2026-03-08 13:18:41 -0300

    Arrumando JS quebrado

--- js/script.js
+++ js/script.js
@@ -110,25 +110,14 @@ function enviarReserva() {
     const obsTexto = obs.trim() !== "" ? `\n📝 *OBS:* ${obs}` : "";
 
     const mensagem = encodeURIComponent(
-<<<<<<< HEAD
-        `🌴 *NOVA RESERVA - EXPLORA BF* 🌴\n\n` +
-        `👤 *Nome:* ${nome}\n` +
-        `📱 *WhatsApp:* ${tel}\n` +
-        `📅 *Data:* ${data}\n` +
-        `👥 *Pessoas:* ${qtd}\n\n` +
-        `📍 *ROTEIRO SELECIONADO:*\n${itensTexto}\n` +
-        `💰 *TOTAL ESTIMADO:* ${total}\n\n` +
-        `${obsTexto}`
-=======
-        `*NOVA RESERVA - EXPLORA BF*\n\n` +
+        ` *NOVA RESERVA - EXPLORA BF* 🌴\n\n` +
         ` *Nome:* ${nome}\n` +
         ` *WhatsApp:* ${tel}\n` +
         ` *Data:* ${data}\n` +
         ` *Pessoas:* ${qtd}\n\n` +
-        ` *ROTEIRO SELECIONADO:*\n${itensTexto}` +
-        obsTexto +
-        `\n *TOTAL ESTIMADO:* ${total}\n`
->>>>>>> 88ca6a3650370046e55016de45fc6ba2b75bc992
+        ` *ROTEIRO SELECIONADO:*\n${itensTexto}\n` +
+        ` *TOTAL ESTIMADO:* ${total}\n\n` +
+        `${obsTexto}`
     );
 
     const numeroWhats = "5584991951206";
